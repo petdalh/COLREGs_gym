2bcb69825d3deb509949e86538c8e7b1725f58f4
Petter Dalhaug <124355644+petdalh@users.noreply.github.com>
2026-03-31 15:16:22 -0700

    moved robustness calculation into it's own util file

--- colregs_gym.py
+++ colregs_gym.py
@@ -8,10 +8,9 @@ collision detection, and pacSTL robustness evaluation.
 import numpy as np
 from mchorcrux.numpy_core.gym.mc_gym_csad_numpy import McGym
 from pacstl.common.interfaces import PACReachableSet, TimeStampedState
-from pacstl.domains.colregs.utils import VesselModel
-from pacstl.core.evaluator import PacSTLEvaluator
 from mchorcrux.numpy_core.controllers.adaptive_seakeeping import heading_to_goal, MRACShipController
 from vessel_utils import cross_track_error, to_obstacle_frame, wrap_angle, compute_monitoring_radius, within_monitoring_radius
+from robustness_utils import evaluate_robustness
 
 
 # Discrete action space
@@ -303,7 +302,13 @@ class ColregsGym(McGym):
 
         if self._step_count % self.robustness_sampling_rate == 0:
             if in_radius:
-                robustness = self.evaluate_robustness()
+                robustness = evaluate_robustness(
+                    spec=self.spec,
+                    ellipsoids_Ab_dict=self.ellipsoids_Ab_dict,
+                    encounter_vessel_eta=self.encounter_vessel_eta,
+                    state=self.get_state(),
+                    encounter_speed=self.encounter_speed,
+                )
                 info["robustness"] = robustness
                 self._update_encounter_state(robustness)
             else:
@@ -362,71 +367,6 @@ class ColregsGym(McGym):
     # ------------------------------------------------------------------
     # pacSTL monitoring
     # ------------------------------------------------------------------
-
-    def evaluate_robustness(self):
-        """Evaluate the pacSTL specification over the prediction horizon."""
-        if self.spec is None or self.ellipsoids_Ab_dict is None:
-            return None
-        if self.encounter_vessel_eta is None:
-            return None
-
-        state = self.get_state()
-        eta, nu = state["eta"], state["nu"]
-        psi_ego = eta[-1]
-        u, v_sway = nu[0], nu[1]
-
-        # World-frame velocities of ego
-        ego_vn = u * np.cos(psi_ego) - v_sway * np.sin(psi_ego)
-        ego_ve = u * np.sin(psi_ego) + v_sway * np.cos(psi_ego)
-
-        # Obstacle current state
-        obs_n, obs_e, obs_psi = self.encounter_vessel_eta
-        obs_vn = self.encounter_speed * np.cos(obs_psi)
-        obs_ve = self.encounter_speed * np.sin(obs_psi)
-
-        ego_trajectory = {}
-        reachable_tube = {}
-
-        for time_step, raw_tuple in self.ellipsoids_Ab_dict.items():
-            A, b, c = raw_tuple
-
-            # Predict ego position in world frame at this time step
-            ego_n = eta[0] + ego_vn * time_step
-            ego_e = eta[1] + ego_ve * time_step
-
-            # Predict obstacle position in world frame at this time step
-            pred_obs_n = obs_n + obs_vn * time_step
-            pred_obs_e = obs_e + obs_ve * time_step
-
-            local_pos, local_vel = to_obstacle_frame(
-                ego_pos=np.array([ego_n, ego_e]),
-                ego_vel=np.array([ego_vn, ego_ve]),
-                obs_pos=np.array([pred_obs_n, pred_obs_e]),
-                obs_psi=obs_psi,
-                obs_vel=np.array([obs_vn, obs_ve]),
-            )
-            local_x, local_y = local_pos
-
-            # Relative heading (same as original)
-            psi_rel = -wrap_angle(psi_ego - obs_psi)
-            local_vx, local_vy = local_vel
-            speed = np.hypot(local_vx, local_vy)
-
-            # 6D state in obstacle-relative frame:
-            # [p_x, p_y, psi_rel, v_x, v_y, |v|]
-            state_array = np.array([
-                local_x, local_y, psi_rel,
-                local_vx, local_vy, speed
-            ])
-
-            ego_trajectory[time_step] = TimeStampedState(
-                time_step=time_step, state_array=state_array
-            )
-            reachable_tube[time_step] = PACReachableSet(
-                time_step=time_step, A_matrix=A, b_vector=b, center=c
-            )
-
-        return self.spec.evaluate(reachable_tube, ego_trajectory)
 
     # ------------------------------------------------------------------
     # Action masking and decoding
